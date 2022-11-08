9ed92fdb0a255b88d582265ef2c8ff19b215415f
VTrofym <v.trof@icloud.com>
2022-11-08 22:46:46 +0200

    start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,6 @@ import css from './App.module.css';
 export const App = () => {
   return (
     <div className={css.allContent}>
-      <div>qwe</div>
       <h1 className={css.title}>Phonebook</h1>
       <ContactForm />
       <h2 className={css.contacts}>Contacts</h2>
