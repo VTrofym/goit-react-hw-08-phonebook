2284e793bfdedc82dc7c1e4c475b1246298600cb
VTrofym <v.trof@icloud.com>
2022-11-08 23:12:05 +0200

    added persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReduser";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const persistedReducer = rootReducer
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['contacts'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
 });
 
+export let persistor = persistStore(store)
